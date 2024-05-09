7a126ba68e5d759148d5ad1c54063540668d969e
deveshbis <deveshbiswas.cse@gmail.com>
2024-05-09 23:33:02 +0600

    Create a 404 page

--- src/layouts/Routes.jsx
+++ src/layouts/Routes.jsx
@@ -2,12 +2,14 @@ import {
     createBrowserRouter
   } from "react-router-dom";
 import Roots from "../Root/Roots";
+import ErrorPage from "../pages/ErrorPage";
 
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Roots></Roots>,
+      errorElement: <ErrorPage></ErrorPage>
     },
   ]);
 
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            404 Error Page
+        </div>
+    );
+};
+
+export default ErrorPage;
