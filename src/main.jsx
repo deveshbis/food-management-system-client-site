28287eeca5a5b6214dbd0eef1843ae695c07ec09
deveshbis <deveshbiswas.cse@gmail.com>
2024-05-13 18:06:24 +0600

    the mutation feature of tanstack query.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { RouterProvider } from 'react-router-dom'
 import router from './layouts/Routes.jsx'
 import FirebaseProvider from './firebase/FirebaseProvider.jsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 
 const queryClient = new QueryClient()
 
@@ -13,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <FirebaseProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </FirebaseProvider>
   </React.StrictMode>,
